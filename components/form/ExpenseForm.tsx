a7d504bbab02bca0db70f3f79c2f0f0a55eb784a
Behzad Aslani <behzadaslani80@gmail.com>
2024-06-04 17:37:03 +0330

    feat: implemented form functionality
    form validation is not implemented yet

--- components/form/ExpenseForm.tsx
+++ components/form/ExpenseForm.tsx
@@ -1,13 +1,30 @@
 import { View, Text } from 'tamagui'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import CustomInput from './CustomInput'
+import { CustomButton } from 'components/UI/CustomButton'
+import { ExpensesContext } from 'store/expensesContext'
+import { Expense } from 'components/expenses/ExpenseItem'
 
-const ExpenseForm = () => {
+type Props = {
+  isEditing: boolean
+  cancelHandler: () => void
+  onSubmit: (expense: Omit<Expense, 'id'>) => void
+  expenseId?: string
+}
+
+const ExpenseForm = ({
+  isEditing,
+  cancelHandler,
+  expenseId,
+  onSubmit,
+}: Props) => {
+  const { addExpense, updateExpense } = useContext(ExpensesContext)
   const [inputValues, setInputValues] = useState({
     amount: '',
     date: '',
-    description: '',
+    title: '',
   })
+
   const inputChangeHandler = (identifier: string, enteredValue: string) => {
     setInputValues((currentValues) => {
       return {
@@ -16,6 +33,17 @@ const ExpenseForm = () => {
       }
     })
   }
+
+  const submitHandler = () => {
+    const expenseData = {
+      amount: +inputValues.amount,
+      date: new Date(inputValues.date),
+      title: inputValues.title,
+    }
+
+    onSubmit(expenseData)
+  }
+
   return (
     <View mt={40} gap={16}>
       <Text fos={24} fow={'bold'} color={'#ffffff'} textAlign='center' mb={8}>
@@ -33,13 +61,29 @@ const ExpenseForm = () => {
         <CustomInput
           label='Date'
           inputConfig={{
+            value: inputValues.date,
             placeholder: 'yyyy-mm-dd',
             maxLength: 10,
             onChangeText: (value) => inputChangeHandler('date', value),
           }}
         />
       </View>
-      <CustomInput label='Description' inputConfig={{ multiline: true }} />
+      <CustomInput
+        label='Description'
+        inputConfig={{
+          multiline: true,
+          value: inputValues.title,
+          onChangeText: (value) => inputChangeHandler('title', value),
+        }}
+      />
+      <View fd={'row'} jc={'center'} ai={'center'} gap={16}>
+        <CustomButton type='outlined' onPress={cancelHandler} minWidth={120}>
+          Cancel
+        </CustomButton>
+        <CustomButton onPress={submitHandler} minWidth={120}>
+          {isEditing ? 'Update' : 'Add'}
+        </CustomButton>
+      </View>
     </View>
   )
 }
--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -2,6 +2,7 @@ import { StackScreenProps } from '@react-navigation/stack'
 import { colors } from 'assets/colors'
 import { CustomButton } from 'components/UI/CustomButton'
 import IconButton from 'components/UI/IconButton'
+import { Expense } from 'components/expenses/ExpenseItem'
 import ExpenseForm from 'components/form/ExpenseForm'
 import { RootStackParamList } from 'navigation'
 import React, { FC, useContext, useLayoutEffect } from 'react'
@@ -33,31 +34,22 @@ const ManageExpense: FC<Props> = ({ route, navigation }) => {
     navigation.goBack()
   }
 
-  const confitmHandler = () => {
+  const confitmHandler = (expenseData: Omit<Expense, 'id'>) => {
     if (editExpenseId) {
-      updateExpense({
-        title: 'ssd',
-        amount: 30,
-        date: new Date('2024-05-23'),
-        id: editExpenseId,
-      })
+      updateExpense({ ...expenseData, id: editExpenseId })
     } else {
-      addExpense({ title: 'mobile', amount: 300, date: new Date('2024-04-13') })
+      addExpense(expenseData)
     }
     navigation.goBack()
   }
 
   return (
-    <View f={1} p={24} bg={colors.primary800} gap={24}>
-      <ExpenseForm />
-      <View fd={'row'} jc={'center'} ai={'center'} gap={16}>
-        <CustomButton type='outlined' onPress={cancelHandler} minWidth={120}>
-          Cancel
-        </CustomButton>
-        <CustomButton onPress={confitmHandler} minWidth={120}>
-          {isEditing ? 'Update' : 'Add'}
-        </CustomButton>
-      </View>
+    <View f={1} p={24} bg={colors.primary800}>
+      <ExpenseForm
+        onSubmit={confitmHandler}
+        isEditing={isEditing}
+        cancelHandler={cancelHandler}
+      />
       {isEditing && (
         <View mt={16} pt={8} btw={2} btc={colors.primary200} ai={'center'}>
           <IconButton

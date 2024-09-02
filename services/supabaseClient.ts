421e3d98ee495049d90adf9f4b3ffcf33a55d65c
behzad aslani <behzadaslani80@gmail.com>
2024-09-02 18:42:54 +0330

    fixed: used env file for supabase key

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zefnkinlqvrwhvopwmfr.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZm5raW5scXZyd2h2b3B3bWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4MDE2MzUsImV4cCI6MjAzNDM3NzYzNX0.tbl3cjRmtSjTePLGuPmDQIPT06sGB6aM6iv_i_pzeDY'
+const supabaseAnonKey = process.env.EXPO_PUBLIC_API_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey!)
